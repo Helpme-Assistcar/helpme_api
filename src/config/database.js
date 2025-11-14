3b6e96929ce636a2bb99bce9e7f6981d58f23e6c
M7uricio <m7uricio@gmail.com>
2025-11-14 16:33:07 -0300

    add: Updates API to support PostgreSQL.

--- src/config/database.js
+++ src/config/database.js
@@ -1,21 +1,26 @@
 require("dotenv").config();
 
-module.exports = {
-  dialect: "mysql",
-  // dialect: "postgres",
-  dialectOptions: {
-    decimalNumbers: true,
-  },
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-  pool: {
-    max: 20,
-    min: 0,
-    idle: 300000,
-    acquire: 60000,
-  },
-  logging: false,
-};
+const isProduction = process.env.NODE_ENV === "production";
+
+module.exports = isProduction
+  ? {
+      dialect: "postgres",
+      use_env_variable: "DATABASE_URL",
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    }
+  : {
+      dialect: "mysql",
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT,
+      dialectOptions: { decimalNumbers: true },
+      logging: false,
+    };
