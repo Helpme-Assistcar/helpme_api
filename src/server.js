80c4143dcaeaac5d3dba96f8cf542991fadc1708
M7uricio <m7uricio@gmail.com>
2025-11-15 17:39:25 -0300

    fix: server port

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 const appInstance = require("./app");
 
-const PORT = process.env.SERVER_PORT || 8088;
+const PORT = process.env.SERVER_PORT || 8080; // Render fornece PORT automaticamente
 
-appInstance.server.listen(PORT, () => {
+appInstance.server.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
